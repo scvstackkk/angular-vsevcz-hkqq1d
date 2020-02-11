32e4de1c45730d2b1675de6b33a1df1bc4556766
scvstackkk <scv.stack@gmail.com>
2020-02-10 18:24:49 -0600

    Added cart component to list of declarations in @NgModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,7 +25,8 @@ import { CartComponent } from './cart/cart.component';
     TopBarComponent,
     ProductListComponent,
     ProductAlertsComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    CartComponent
   ],
   bootstrap: [ AppComponent ]
 })
